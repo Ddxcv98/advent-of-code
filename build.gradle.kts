6aacb6315af998e0d632a3020f35999cb8a41bce
David Duarte <no@thank.you>
2023-12-05 23:23:53 +0000

    Upgrade java, kotlin and gradle versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "1.9.21"
 }
 
 group = "org.example"
@@ -21,5 +21,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "21"
 }
